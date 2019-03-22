ba28aa159d850566a49e7a048a263d1e5d6a745a
Gavin Smith <gavin.smith@nottingham.ac.uk>
2019-03-22 12:41:46 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+long_description = """TODO
+"""
+
+setup(
+    name='rstldecompose',
+    version='0.1',
+    url='https://github.com/nlabmsc/rstldecompose',
+    license='NONE',
+    py_modules=['rstldecompose'],
+    author='Gavin Smith',
+    author_email='gavin.smith@nottingham.ac.uk',
+    install_requires=[],
+    description='N/A',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='',
+    classifiers=['Intended Audience :: NLAB MSc']
+)
